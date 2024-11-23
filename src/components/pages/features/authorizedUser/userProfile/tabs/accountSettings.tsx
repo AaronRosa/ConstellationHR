8d0c9d9bdd167fd512d7926061cacd79e82a447e
Aaron Rosa <a_rosa6942@yahoo.com>
2024-11-23 00:10:05 -0600

    Updating user profile

--- src/components/pages/features/authorizedUser/userProfile/tabs/accountSettings.tsx
+++ src/components/pages/features/authorizedUser/userProfile/tabs/accountSettings.tsx
@@ -1,45 +1,116 @@
-import React from 'react';
-
-import { TextField, Button, Grid, Box } from '@mui/material';
+import React, { useState, ChangeEvent, FormEvent } from "react";
+import {
+    TextField,
+    Button,
+    Grid,
+    Typography,
+    Box,
+    Paper,
+} from "@mui/material";
 import { useSelector } from 'react-redux';
 import { selectCurrentUser } from '../../../../../../redux/slices/userSlice';
+
+// Define the type for the form data
+interface FormData {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+}
 
 const AccountSettingsForm: React.FC = () => {
 
     //get user data from redux store
     const user = useSelector(selectCurrentUser);
 
-    const handleSubmit = (event: React.FormEvent) => {
-        event.preventDefault();
-        // Handle form submission logic here
+    // State with type annotation
+    const [formData, setFormData] = useState<FormData>({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+    });
+
+    // Handle input change with type annotations
+    const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    // Handle form submission with type annotations
+    const handleSubmit = (e: FormEvent): void => {
+        e.preventDefault();
+        console.log("Form Submitted:", formData);
     };
 
     return (
-        <Box>
-            <h2>Account Settings</h2>
+        <div>
+            <Typography variant="h5" gutterBottom>
+                Account Management
+            </Typography>
             <form onSubmit={handleSubmit}>
                 <Grid container spacing={2}>
-                    <Grid item xs={12} sm={6}>
-                        <TextField label="First Name" variant="outlined" defaultValue={user.fname} />
+                    <Grid item xs={12} md={6}>
+                        <TextField
+                            fullWidth
+                            label="First Name"
+                            name="firstName"
+                            defaultValue={user.fname}
+                            onChange={handleChange}
+                            variant="outlined"
+                            required
+                        />
                     </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <TextField label="Last Name" variant="outlined" defaultValue={user.lname} />
+                    <Grid item xs={12} md={6}>
+                        <TextField
+                            fullWidth
+                            label="Last Name"
+                            name="lastName"
+                            defaultValue={user.lname}
+                            onChange={handleChange}
+                            variant="outlined"
+                            required
+                        />
                     </Grid>
                     <Grid item xs={12}>
-                        <TextField label="Email" variant="outlined" defaultValue={user.email} />
+                        <TextField
+                            fullWidth
+                            label="Email"
+                            name="email"
+                            defaultValue={user.email}
+                            onChange={handleChange}
+                            type="email"
+                            variant="outlined"
+                            required
+                        />
                     </Grid>
                     <Grid item xs={12}>
-                        <TextField label="Password" variant="outlined" type="password" />
+                        <TextField
+                            fullWidth
+                            label="Password"
+                            name="password"
+                            onChange={handleChange}
+                            type="password"
+                            variant="outlined"
+                            required
+                        />
                     </Grid>
                     <Grid item xs={12}>
-                        <Button type="submit" variant="contained" color="primary">
-                            Save
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                        >
+                            Submit
                         </Button>
                     </Grid>
                 </Grid>
             </form>
-        </Box>
-
+        </div>
     );
 };
 
--- src/components/pages/features/authorizedUser/userProfile/userProfile.tsx
+++ src/components/pages/features/authorizedUser/userProfile/userProfile.tsx
@@ -1,59 +1,73 @@
-import { useSelector } from "react-redux";
-import { selectCurrentUser } from "../../../../../redux/slices/userSlice";
-import {
-  Grid,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-} from "@mui/material";
-import { Settings, Mail, Notifications } from "@mui/icons-material";
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import AccountSettingsForm from './tabs/accountSettings';
+import Mail from './tabs/mail';
 
-import AccountSettings from "./tabs/accountSettings";
-
-const userMenu = <Grid sm={12} md={3}>
-  <List style={{ float: "left" }}>
-    <ListItem>
-      <ListItemButton>
-        <ListItemIcon>
-          <Settings />
-        </ListItemIcon>
-        Account Settings
-      </ListItemButton>
-    </ListItem>
-    <ListItem>
-      <ListItemButton>
-        <ListItemIcon>
-          <Mail />
-        </ListItemIcon>
-        Mail
-      </ListItemButton>
-    </ListItem>
-    <ListItem>
-      <ListItemButton>
-        <ListItemIcon>
-          <Notifications />
-        </ListItemIcon>
-        Notifications
-      </ListItemButton>
-    </ListItem>
-  </List>
-</Grid>;
-export function UserProfile(props: any) {
-  const loggedInUser = useSelector(selectCurrentUser);
-
-  if (loggedInUser) {
-    return (
-      <Grid container spacing={1}>
-        {userMenu}
-        <Grid sm={12} md={9}  >
-          <div style={{ paddingTop: "8px", paddingBottom: "8px" }}>
-            <AccountSettings />
-          </div>
-        </Grid>
-      </Grid>
-    );
-  } else return null;
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
 }
 
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+function UserProfile() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box
+      sx={{ flexGrow: 1, display: 'flex' }}
+    >
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        sx={{ borderRight: 1, borderColor: 'divider' }}
+      >
+        <Tab label="Account Settings" {...a11yProps(0)} />
+        <Tab label="Mail" {...a11yProps(1)} />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        <AccountSettingsForm />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <Mail />
+      </TabPanel>
+    </Box>
+  );
+}
 export default UserProfile;
